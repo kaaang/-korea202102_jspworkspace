f1ae4e26447cbb314216af82110fddb7321c77c9
kaaang <kaaang@kakao.com>
2021-07-07 15:07:56 +0900

    0707

---
+++ SpringMVC0707/src/main/java/com/koreait/springmvc0707/model/board/service/BoardServiceImpl.java
@@ -0,0 +1,42 @@
+package com.koreait.springmvc0707.model.board.service;
+
+import java.util.List;
+
+import org.apache.catalina.Service;
+
+import com.koreait.springmvc0707.model.domain.Board;
+
+//서비스를 구현한 객체
+public class BoardServiceImpl implements BoardService{
+
+	@Override
+	public List selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Board select(int board_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int board_id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
