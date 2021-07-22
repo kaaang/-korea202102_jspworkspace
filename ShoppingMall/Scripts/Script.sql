db47355101b6c3f745b3cecc428f54c447779bb0
kaaang <kaaang@kakao.com>
2021-07-22 17:51:54 +0900

    0722

--- ShoppingMall/Scripts/Script.sql
+++ ShoppingMall/Scripts/Script.sql
@@ -10,3 +10,15 @@ order by t.topcategory_id asc;
 select * from subcategory where topcategory_id =(
 	select topcategory_id from topcategory where top_name='Down'
 );
+
+
+
+
+create table product(
+	product_id int primary key auto_increment 
+	, subcategory_id int 
+	, product_name varchar(30)
+	, brand varchar(25)
+	, price int default 0 
+	, info text
+) default character set utf8;
